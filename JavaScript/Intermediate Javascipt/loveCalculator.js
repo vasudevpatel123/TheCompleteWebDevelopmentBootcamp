b6240368ad3ea349e01fc38000f70dddc5ea11cb
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2023-11-06 15:25:05 +0530

    Create loveCalculator.js

---
+++ JavaScript/Intermediate Javascipt/loveCalculator.js
@@ -0,0 +1,6 @@
+var name1 = prompt("Enter the first person name : ");
+var name2 = prompt("Enter the second person name : ");
+var n = Math.random();
+var loveScore = n * 100;
+var loveScore = Math.floor(loveScore)+1;
+alert("Your love score is : "+loveScore+"%");
