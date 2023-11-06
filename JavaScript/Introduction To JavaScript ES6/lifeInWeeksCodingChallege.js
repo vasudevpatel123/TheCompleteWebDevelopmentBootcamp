dba7224f04ca8ee4ead922130153d415e7fe7c00
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2023-11-06 13:12:26 +0530

    Create lifeInWeeksCodingChallege.js

---
+++ JavaScript/Introduction To JavaScript ES6/lifeInWeeksCodingChallege.js
@@ -0,0 +1,17 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    
+  var ageLeftInYears = 90 - age;
+  var x = ageLeftInYears * 365;
+  var y = ageLeftInYears * 52;
+  var z = ageLeftInYears * 12;
+  console.log("You have " + x + " days, " + y + " weeks, and " + z + " months left.")
+    
+    
+    
+/*************Don't change the code below**********/
+}
+
