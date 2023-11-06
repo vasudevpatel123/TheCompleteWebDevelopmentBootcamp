92c6920524cd0bf80887bbece53cc3698935d6cd
vasudevpatel123 <44128933+vasudevpatel123@users.noreply.github.com>
2023-11-06 18:45:19 +0530

    Create randomPersonBuyingLunch.js

---
+++ JavaScript/Intermediate Javascipt/randomPersonBuyingLunch.js
@@ -0,0 +1,15 @@
+function whosPaying(names) {
+    
+/******Don't change the code above*******/
+    
+    //Write your code here.
+    
+    
+    var selectedPerson = names[Math.floor(Math.random() * names.length)];
+    return (selectedPerson+" is going to buy lunch today!")
+    
+    
+
+
+/******Don't change the code below*******/    
+}
